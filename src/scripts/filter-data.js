0a3f7df690e7d76742cdc2ae9c941989461d2920
Ramón Guijarro <hola@soyguijarro.com>
2017-05-01 10:18:24 +0200

    Refactor data filtering functions

--- src/scripts/filter-data.js
+++ src/scripts/filter-data.js
@@ -1,12 +1,9 @@
 import compose from 'ramda/src/compose';
 import filter from 'ramda/src/filter';
 
-const testString = regExp => string => regExp.test(string);
+const testString = refString => string => new RegExp(refString, 'ig').test(string);
 
 const getItemString = item => item.name + item.content;
 
-export default (data, searchString) => {
-  const testForSearchString = testString(new RegExp(searchString, 'ig'));
-
-  return filter(compose(testForSearchString, getItemString), data);
-};
+export default (data, searchString) =>
+  filter(compose(testString(searchString), getItemString), data);
